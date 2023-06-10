5f4036c9cb7952a4144d031c6549d2f1c18987a7
Hrishikesh <dhormarehrishi10@gmail.com>
2023-06-10 15:34:35 +0530

    passing data as argument

--- src/main.js
+++ src/main.js
@@ -1,16 +1,25 @@
 import { MongoClient } from "mongodb";
 
-async function main() {
+async function insertRecord(jsonDocument) {
   const uri = "mongodb://127.0.0.1:27017";
   const client = new MongoClient(uri);
 
   const db = client.db("mydb");
   const message = db.collection("message");
 
-  await message.insert({ message: "Data from mongodb.js" });
+  await message.insertOne(jsonDocument);
   await client.close();
 
   console.log("Record Added Successfully");
+}
+
+async function main(){
+    let jsonDocument = {
+        message: "Hello Universe",
+        to: "santosh",
+        from: "tejas",
+    }
+    await insertRecord(jsonDocument);
 }
 
 main();
